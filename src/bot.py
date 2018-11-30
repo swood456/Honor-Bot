124552cb399e5eab4fa61571c8de7824a6e8b53b
swood456 <smwood456@gmail.com>
2018-11-29 23:55:14 -0500

    Converted to using MongoDB to store user honor. Re-implemented single user and all user honor

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,9 @@
 # Work with Python 3.6
 import random
 import asyncio
-import aiohttp
 import json
 import os
+import pymongo
 from discord import Game
 from discord.ext.commands import Bot
 
@@ -16,22 +16,28 @@ from discord.ext.commands import Bot
 K_INITIAL_USER_HONOR = 100.0
 BOT_PREFIX = ("!")
 
-# with open(os.path.join(os.pardir, 'data', 'auth.json')) as auth_file:
+# Load auth token
 with open(os.path.join('data', 'auth.json')) as auth_file:
     TOKEN = json.load(auth_file)['token']
 
+# load statuses
 with open(os.path.join('data', 'statuses.json')) as status_file:
     statuses = json.load(status_file)['statuses']
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-# name is the main way to use command
-# description is the long explaination of what the thing does when getting help for that specific command
-# brief is the info in default help command
-# aliases are other things that work for command
-# pass_context I think gives info about message/server/etc.
+# Load honor stuff from mongo
+mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 
-honor_pool = {}
+honorBot_db = mongoClient.honorBot
+
+user_collection = honorBot_db.users
+
+class HonorBet:
+    def __init__(self, value = 10, player1 = None, player2 = None):
+        self.value = value
+        self.player1 = player1
+        self.player2 = player2
 
 # example thing don't keep
 @client.command(name='8ball',
@@ -49,12 +55,6 @@ async def eight_ball(context):
     ]
     await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
-# Testing basic command, remove eventually
-@client.command()
-async def square(number):
-    squared_value = int(number) * int(number)
-    await client.say(str(number) + " squared is " + str(squared_value))
-
 @client.command(name='honor',
                 desciption='Gives the amount of honor for the given user. Name is case sensitive and accepts nickname or username',
                 brief='Gives honor of user',
@@ -64,10 +64,11 @@ async def list_honor(context, name):
     member = context.message.server.get_member_named(name)
 
     if member:
-        await client.say(member.display_name + ' has ' + str(int(honor_pool[context.message.server.id][member.id])) + ' honor')
+        check_user(member)
+        member_honor = user_collection.find_one({ '_id': member.id })
+        await client.say(member.display_name + ' has ' + str(member_honor['honor']) + ' honor')
     else:
         await client.say(name + ' not recognized as a user on this server. Make sure capitalization is correct and try again')
-
 
 @client.command(name='allHonor',
                 description='Lists the honor of all users on the server. If there are more than 20 users, it will only list 20',
@@ -80,12 +81,14 @@ async def all_honor(context):
     count = 0
     message = '```\n'
 
-    for mem in server.members:
-        if mem.id in honor_pool[server.id]:
-            message += mem.display_name + ': ' + str(int(honor_pool[server.id][mem.id])) + '\n'
-            count += 1
-            if count >= 20:
-                break
+    for member in server.members:
+        if member.bot: continue
+        check_user(member)
+        member_honor = user_collection.find_one({ '_id': member.id })['honor']
+        message += member.display_name + ': ' + str(member_honor) + '\n'
+        count += 1
+        if count >= 20:
+            break
 
     message += '```'
 
@@ -96,20 +99,29 @@ async def all_honor(context):
 # TODO: command for listing all honor bets that I am a part of
 
 # TODO: command for creating an honor bet
+@client.command(name='makeBet',
+                description='Creates a new honor bet for another user to accept. You may specify a value for the bet',
+                brief='Create a new honor bet for another user to accept',
+                alias=['createBet', 'make_bet', 'create_bet', 'newBet', 'new_bet'],
+                pass_context='true')
+async def make_bet(context):
+    await client.say('this still needs to be implemented OOPS')
 
 @client.check
 def check_global(context):
-    server = context.message.server
-    if server.id not in honor_pool:
-        honor_pool[server.id] = {}
+    return check_user(context.message.author)
 
-        for member in server.members:
-            if not member.bot:
-                honor_pool[server.id][member.id] = K_INITIAL_USER_HONOR
-    # Make sure the author is in the database
-    elif context.message.author.id not in honor_pool[server.id]:
-        honor_pool[server.id][context.message.author.id] = K_INITIAL_USER_HONOR
+def check_user(member):
+    result = user_collection.count_documents({ '_id': member.id })
+    if result <= 0:
+        add_new_user(member)
     return True
+
+def add_new_user(member):
+    user_collection.insert_one({
+        "_id": member.id,
+        "honor": K_INITIAL_USER_HONOR
+    })
 
 @client.event
 async def on_ready():
